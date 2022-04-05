3ff85693193775f6d120659cc12db6108a4e1c95
ciera <cieranicole.m15@gmail.com>
2022-04-05 09:46:55 -0400

    Completed lab

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCase)
+  function titleCase(str){
+    return str.split(' ').map((word) => 
+   word[0].toUpperCase() + word.slice(1)).join(' ')
+ }
 }
+
+
+
+
